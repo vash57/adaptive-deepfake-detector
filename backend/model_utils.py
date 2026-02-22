b7e156e5d5bf09e5686cda95d86a96f93d505ce7
DKC <devchaubey03@gmail.com>
2026-02-22 22:44:57 +0530

    Refactor model loading to use lazy initialization
    Refactor model loading to load only once and improve efficiency.

--- backend/model_utils.py
+++ backend/model_utils.py
@@ -2,29 +2,30 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 
-# -------- Model Config --------
 MODEL_PATH = "model.keras"
 IMG_SIZE = 224
 
-model = tf.keras.models.load_model(MODEL_PATH)
-print("✅ Model loaded successfully")
+model = None
+
+def load_model_once():
+    global model
+    if model is None:
+        model = tf.keras.models.load_model(MODEL_PATH)
+        print("✅ Model loaded successfully")
 
 
-# -------- Prediction Function --------
 def predict_image(image_path):
 
-    # image load
+    load_model_once()
+
     img = Image.open(image_path).convert("RGB")
     img = img.resize((IMG_SIZE, IMG_SIZE))
 
-    # preprocessing
     img = np.array(img) / 255.0
     img = np.expand_dims(img, axis=0)
 
-    # prediction
     pred = model.predict(img, verbose=0)[0][0]
 
-    # decision
     if pred > 0.5:
         result = "Real Image"
     else:
